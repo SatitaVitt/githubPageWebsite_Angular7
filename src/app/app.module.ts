97f8c625a0412e187df9f6ae46a9f2d165342eda
SatitaVitt <sv439@scarletmail.rutgers.edu>
2020-08-25 11:45:45 -0400

    register fakebackend auth jwt error interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule , HTTP_INTERCEPTORS} from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
 
 import { AppComponent } from './app.component';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
 import { appRoutingModule } from './app.routing';
-import {LoginComponent} from './login';
+import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 
 @NgModule({
-    imports: [BrowserModule, appRoutingModule],
+    imports: [
+        BrowserModule, 
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
     declarations: [
         AppComponent,
         HomeComponent,
         LoginComponent,
         RegisterComponent
        
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        // provider used to create fake backend
+        fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
+import { AuthGuard } from './_helpers';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: HomeComponent , canActivate: [AuthGuard] },
+    /*To control access to a route with the auth guard you add it to the canActivate array 
+    in the route's configuration. The route guards in the canActivate array are run by Angular 
+    to decide if the route can be "activated", if all of the route guards return true navigation 
+    is allowed to continue, but if any of them return false navigation is cancelled.
+    */
+
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
